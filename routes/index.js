3a428258f591d652ea0ba4a188743973fd31bac5
Joe Brock <lonedawg@madhamster.org>
2015-11-19 00:01:48 +0000

    Stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var mongo = require('mongodb');
-var monk = require('monk');
-
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
