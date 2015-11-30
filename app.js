3cd2f8085c0a5df6b6d7450ba6f692d31285f338
Joe Brock <lonedawg@madhamster.org>
2015-11-30 14:34:26 +0000

    Theming mostly.

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var db = monk('mongodb://d3vuser:caltinea@ds047504.mongolab.com:47504/guzzlr');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -126,6 +127,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', routes);
+// app.use('/api', api);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
